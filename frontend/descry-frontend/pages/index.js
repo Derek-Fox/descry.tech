2ce3a186b341576adb15332d1b9ca85e72b60988
kenny <kennycs@protonmail.com>
2023-04-08 13:49:16 -0400

    Added additional algorithms

--- frontend/descry-frontend/pages/index.js
+++ frontend/descry-frontend/pages/index.js
@@ -4,6 +4,9 @@ import Image from 'next/image'
 import code1 from '../public/code1.svg'
 import code2 from '../public/code2.svg'
 import code3 from '../public/code3.svg'
+import code4 from '../public/code4.svg'
+import code5 from '../public/code5.svg'
+import code6 from '../public/code6.svg'
 
 import {Inter} from 'next/font/google'
 import styles from '@/styles/Home.module.css'
@@ -33,6 +36,9 @@ export default function Home() {
                     <AlgorithmBox name={"QuickSort"} image={code1}/>
                     <AlgorithmBox name={"MergeSort"} image={code2}/>
                     <AlgorithmBox name={"Binary Search"} image={code3}/>
+                    <AlgorithmBox name={"Insertion Sort"} image={code4}/>
+                    <AlgorithmBox name={"Dijkstra's Algorithm"} image={code5}/>
+                    <AlgorithmBox name={"Depth First Search"} image={code6}/>
                 </div>
 
                 <div className={styles.grid}>
