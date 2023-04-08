56c65fd2d00305194ba0841273e5fcec04789b37
kenny <kennycs@protonmail.com>
2023-04-08 12:17:49 -0400

    Added title text to homepage

--- frontend/descry-frontend/pages/index.js
+++ frontend/descry-frontend/pages/index.js
@@ -15,39 +15,13 @@ export default function Home() {
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+        <div className={styles.navbar}>
+          <div className={styles.title}>
+            <h1>Descry</h1>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
+          <div className={styles.subtitle}>
+            <h2>Algorithms Visualized</h2>
+          </div>
         </div>
 
         <div className={styles.grid}>
