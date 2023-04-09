47537f4e82bfb0cbb1bda0f061614b2e8da9a535
kenny <kennycs@protonmail.com>
2023-04-09 02:48:55 -0400

    Updated launch button size

--- frontend/descry-frontend/components/launch.js
+++ frontend/descry-frontend/components/launch.js
@@ -1,8 +1,9 @@
 import styles from '@/styles/algorithm.module.css'
+
 export default function Launch({file}) {
     return (
-        <a href={file} download={file} download="jar">
-            Download
+        <a href={file} download={file} target="_blank">
+            <button className={styles.launch}>Launch</button>
         </a>
     )
 }
