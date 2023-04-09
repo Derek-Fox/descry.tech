947cad011099565f621bcfce244c4ded1eb5a061
kenny <kennycs@protonmail.com>
2023-04-09 01:01:46 -0400

    Corrected navbar links

--- frontend/descry-frontend/components/navbar.js
+++ frontend/descry-frontend/components/navbar.js
@@ -25,9 +25,9 @@ export default function Navbar() {
 
             <nav className={styles.navbar}>
                 <ul className={styles.menus}>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#usage">Usage</a></li>
+                    <li><a href="../#home">Home</a></li>
+                    <li><a href="../#about">About</a></li>
+                    <li><a href="../#usage">Usage</a></li>
                 </ul>
             </nav>
         </div>
