1976ea58b0f9aae52699270cfb0f6bcc599aeb24
kenny <kennycs@protonmail.com>
2023-04-09 01:06:16 -0400

    Updated links for navbar

--- frontend/descry-frontend/components/navbar.js
+++ frontend/descry-frontend/components/navbar.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import eye from "@/public/eye.png";
 import styles from "@/styles/navbar.module.css";
+import Link from 'next/link'
 
 export default function Navbar() {
     return (
         <div>
-            <div className={styles.header} id="home">
+            <Link href="/" className={styles.header} id="home">
                 <div className={styles.titleBox}>
                     <div className={styles.title}>
                         <h1>Descry</h1>
@@ -21,13 +22,13 @@ export default function Navbar() {
                     width={150}
                     height={150}
                 />
-            </div>
+            </Link>
 
             <nav className={styles.navbar}>
                 <ul className={styles.menus}>
-                    <li><a href="../#home">Home</a></li>
-                    <li><a href="../#about">About</a></li>
-                    <li><a href="../#usage">Usage</a></li>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/#about">About</a></li>
+                    <li><a href="/#usage">Usage</a></li>
                 </ul>
             </nav>
         </div>
